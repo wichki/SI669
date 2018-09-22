d928381d398be283ff1f629a942b7ae81db26667
William Kim <william@Williams-MacBook-Pro.local>
2018-09-22 15:12:21 -0400

    finished first part

---
+++ Lab 3/angular-tour-of-heroees/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Bad', img: "../assets/imgs/test.jpg"},
+  { id: 12, name: 'Narco', img: "../assets/imgs/test.jpg"},
+  { id: 13, name: 'Bombasto', img: "../assets/imgs/test.jpg"},
+  { id: 14, name: 'Celeritas', img: "../assets/imgs/test.jpg"},
+  { id: 15, name: 'Magnet-Oh-No!', img: "../assets/imgs/test.jpg"},
+  { id: 16, name: 'RubberLady', img: "../assets/imgs/test.jpg"},
+  { id: 17, name: 'Dynama', img: "../assets/imgs/test.jpg"},
+  { id: 18, name: 'Madame IQ', img: "../assets/imgs/test.jpg"},
+  { id: 19, name: 'Magma', img: "../assets/imgs/test.jpg"},
+  { id: 20, name: 'Tornado', img: "../assets/imgs/test.jpg"},
+  { id: 21, name: 'Super Super Hero', img: "../assets/imgs/test.jpg"}
+];
