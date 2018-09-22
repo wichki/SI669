55b263da2286fc9a2186854cfd3123ae491873a8
William Kim <william@Williams-MacBook-Pro.local>
2018-09-22 15:28:28 -0400

    extra credit

--- Lab 3/angular-tour-of-heroees/src/app/mock-heroes.ts
+++ Lab 3/angular-tour-of-heroees/src/app/mock-heroes.ts
@@ -1,15 +1,15 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Bad', img: "../assets/imgs/test.jpg"},
-  { id: 12, name: 'Narco', img: "../assets/imgs/test.jpg"},
-  { id: 13, name: 'Bombasto', img: "../assets/imgs/test.jpg"},
-  { id: 14, name: 'Celeritas', img: "../assets/imgs/test.jpg"},
-  { id: 15, name: 'Magnet-Oh-No!', img: "../assets/imgs/test.jpg"},
-  { id: 16, name: 'RubberLady', img: "../assets/imgs/test.jpg"},
-  { id: 17, name: 'Dynama', img: "../assets/imgs/test.jpg"},
-  { id: 18, name: 'Madame IQ', img: "../assets/imgs/test.jpg"},
-  { id: 19, name: 'Magma', img: "../assets/imgs/test.jpg"},
-  { id: 20, name: 'Tornado', img: "../assets/imgs/test.jpg"},
-  { id: 21, name: 'Super Super Hero', img: "../assets/imgs/test.jpg"}
+  { id: 11, name: 'Mr. Bad', img: "../assets/imgs/angela.png"},
+  { id: 12, name: 'Narco', img: "../assets/imgs/batman.png"},
+  { id: 13, name: 'Bombasto', img: "../assets/imgs/dontknow.jpg"},
+  { id: 14, name: 'Celeritas', img: "../assets/imgs/fantastic.jpeg"},
+  { id: 15, name: 'Magnet-Oh-No!', img: "../assets/imgs/greenlatern.jpeg"},
+  { id: 16, name: 'RubberLady', img: "../assets/imgs/ironman.ico"},
+  { id: 17, name: 'Dynama', img: "../assets/imgs/romanov.png"},
+  { id: 18, name: 'Madame IQ', img: "../assets/imgs/thanos.png"},
+  { id: 19, name: 'Magma', img: "../assets/imgs/thor.png"},
+  { id: 20, name: 'Tornado', img: "../assets/imgs/wolverine.jpeg"},
+  { id: 21, name: 'Super Super Hero', img: "../assets/imgs/captainamerica.png"}
 ];
